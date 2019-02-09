036504941a65b025be6a9f0130342d836a1a86ad
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-02-09 21:42:14 +0300

    added DataProvider

--- src/test/java/ParameterizedTest.java
+++ src/test/java/ParameterizedTest.java
@@ -1,49 +1,34 @@
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import static org.junit.Assert.assertEquals;
 import com.mashape.unirest.http.HttpResponse;
 
-import java.util.Arrays;
 
 
 
-@RunWith(Parameterized.class)
+@RunWith(DataProviderRunner.class)
 public class ParameterizedTest {
-    private String location;
-    private String keyName;
-    private String radius;
-    private String rankby;
-    public String address;
-    public String endpoint;
-    public String key;       // захордкожено не используеться
-    public String status;
 
-    public ParameterizedTest(String location, String keyName, String radius, String rankby, String address, String endpoint, String key) {
-        this.location = location;
-        this.keyName = keyName;
-        this.radius = radius;
-        this.rankby = rankby;
-        this.address = address;
-        this.endpoint = endpoint;
-        this.key = key;
-    }
 
-    @Parameterized.Parameters()
-    public static Iterable<Object[]> dataForTest() {
-        return Arrays.asList(new Object[][]{
+    @DataProvider
+    public static Object[][]dataForPositiveTest() {
+        return new Object[][]{
                 {"40.765738,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
                 {"40.765738,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
                 {"40.765738,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
                 {"40.765738,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
                 {"40.765738,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"}
-        });
+        };
     }
 
     @Test
-    public void paramTest() {
+    @UseDataProvider("dataForPositiveTest")
+    public void paramTest1(String location, String keyName, String radius, String rankby, String address, String endpoint, String key) {
         HttpResponse<String> response = null;
         try {
             response = Unirest.get(new JsonHelper().createUrl(location,keyName,radius,rankby,address,endpoint,key))
@@ -55,4 +40,31 @@ public class ParameterizedTest {
         }
 
     }
+
+    @DataProvider
+    public static Object[][]dataForNegativeTest() {
+        return new Object[][]{
+                {"40,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
+                {"40,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
+                {"40,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
+                {"40,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"},
+                {"40,-73.978194", "configKey", "500", "prominence", "https://maps.googleapis.com/maps/api/" , "place/nearbysearch/json", "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ"}
+        };
+    }
+
+    @Test
+    @UseDataProvider("dataForNegativeTest")
+    public void paramTest2(String location, String keyName, String radius, String rankby, String address, String endpoint, String key) {
+        HttpResponse<String> response = null;
+        try {
+            response = Unirest.get(new JsonHelper().createUrl(location,keyName,radius,rankby,address,endpoint,key))
+                    .header("cache-control", "no-cache")
+                    .asString();
+            assertEquals(200, response.getStatus());
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
