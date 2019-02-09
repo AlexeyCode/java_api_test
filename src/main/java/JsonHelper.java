5b2a4b356e02718c2b2f9d14c6d9b13a3fe747b8
erasecorp@hotmail.com <javarushforthosewhoseek>
2019-02-09 22:36:13 +0300

    added DataProvider

--- src/main/java/JsonHelper.java
+++ src/main/java/JsonHelper.java
@@ -1,15 +1,17 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
-import java.io.IOException;
 
 public class JsonHelper {
 
 
 
 
-    public String createUrl(String location, String keyName, String radius, String rankby, String address, String endpoint, String key){
-        String url = firstPart(address,endpoint) + location(location)+ keyName(keyName)+ radius(radius) + rankby(rankby);
+    public String createUrl(String address, String endpoint, String location,
+                            String radius, String keyword, String rankby, String type,
+                            String key, String minprice, String maxprice, String language){
+
+        String url = firstPart(address,endpoint) + location(location) + radius(radius)
+                + keyword(keyword) + rankby(rankby) + type(type) + key(key)
+                + minprice(minprice) + maxprice(maxprice) + language(language) ;
+
         System.out.println("url: " + url);
         return url;
     }
@@ -18,16 +20,10 @@ public class JsonHelper {
     private String firstPart(String address, String endpoint){
         return address+endpoint+"?";
     }
+
     private String location(String location){
         if(location != null){
             return "location=" + location;
-        }
-        return "";
-    }
-
-    private String keyName(String keyName){
-        if(keyName != null){
-            return "&key=" + "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ";  // вот тут должна быть нормальная подгрузка ключа
         }
         return "";
     }
@@ -38,6 +34,7 @@ public class JsonHelper {
         }
         return "";
     }
+
     private String rankby(String rankby){
         if(rankby != null){
             return "&rankby=" + rankby;
@@ -45,5 +42,46 @@ public class JsonHelper {
         return "";
     }
 
+    private String keyword(String keyword){
+        if(keyword != null){
+            return "&keyword=" + keyword;
+        }
+        return "";
+    }
+
+    private String type(String type){
+        if(type != null){
+            return "&type=" + type;
+        }
+        return "";
+    }
+
+    private String key(String key){
+        if(key != null){
+            return "&key=" + "AIzaSyCWOSz0D-dfNnfv7FJh6pP3dghHM9NmyuQ";  // вот тут должна быть нормальная подгрузка ключа
+        }
+        return "";
+    }
+
+    private String minprice(String minprice){
+        if(minprice != null){
+            return "&minprice=" + minprice;
+        }
+        return "";
+    }
+
+    private String maxprice(String maxprice){
+        if(maxprice != null){
+            return "&maxprice=" + maxprice;
+        }
+        return "";
+    }
+
+    private String language(String language){
+        if(language != null){
+            return "&language=" + language;
+        }
+        return "";
+    }
 
 }
